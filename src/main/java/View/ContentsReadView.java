014745860b3ddba76c6522d9a316d972d88a1f56
nameless0422 <cjy042218@gmail.com>
2021-06-12 17:13:50 +0900

    .

---
+++ src/main/java/View/ContentsReadView.java
@@ -0,0 +1,17 @@
+package View;
+
+import javax.swing.*;
+
+public class ContentsReadView {
+    private JTextArea textArea1;
+    private JPanel MainPanel;
+    private JButton button1;
+    private JTextField textField1;
+    private JButton button2;
+    private JButton button3;
+    private JTable table1;
+
+    public void ContentsReadView(){
+
+    }
+}
