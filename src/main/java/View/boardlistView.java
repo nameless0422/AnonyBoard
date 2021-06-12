77896666d38059b7859294fa74af6bde320ce407
hno130 <hno130@naver.com>
2021-06-12 15:54:48 +0900

    변경사항

---
+++ src/main/java/View/boardlistView.java
@@ -0,0 +1,19 @@
+package View;
+
+import javax.swing.*;
+
+public class boardlistView {
+
+
+    private JButton 가나다Button;
+    private JScrollBar scrollBar1;
+    private JTable t;
+
+    public static void main(String[] args) {
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
