70511c8573a64c84135e07c8518ce4fc343202aa
hno130 <hno130@naver.com>
2021-06-12 20:10:47 +0900

    변경사항

--- src/main/java/Controler/MainProcess.java
+++ src/main/java/Controler/MainProcess.java
@@ -3,18 +3,22 @@ import Model.UserModel;
 import View.ContentsWriteView;
 import View.LoginView;
 
+import View.boardlistView;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import javax.swing.*;
 
 public class MainProcess {
     public static LoginView loginView;
     public static ContentsWriteView contentsWriteView;
+    public static boardlistView BoardlistView;
     public static UserModel User;
     public static void main(String[] args) {
         FlatIntelliJLaf.install();
         MainProcess mainProcess = new MainProcess();
         loginView = new LoginView(mainProcess);
         contentsWriteView = new ContentsWriteView(mainProcess);
+        BoardlistView = new boardlistView(mainProcess);
+        BoardlistView.Visible();
         loginView.Visivle();
     }
 }
--- src/main/java/View/boardlistView.java
+++ src/main/java/View/boardlistView.java
@@ -1,19 +1,52 @@
 package View;
 
+import Controler.MainProcess;
+
 import javax.swing.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+import Controler.MainProcess;
+import Model.UserModel;
+import com.jcraft.jsch.*;
+
 
 public class boardlistView {
 
 
-    private JButton 가나다Button;
-    private JScrollBar scrollBar1;
-    private JTable t;
+    private JButton Button1;
+    private JTable table1;
+    private MainProcess mainProcess;
+    private JPanel panel;
+    private JLabel hiLabel;
+    private JLabel makeLabel;
+    private JFrame frame;
 
-    public static void main(String[] args) {
+    public boardlistView(MainProcess p) {
+        mainProcess = p;
+        frame = new JFrame();
+        frame.setContentPane(panel);
+        frame.setSize(800, 600);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        String header[] = {"강의명","분반","교수명"};
+        String contents[][] = {{"Java프로그래밍","3","양재동"},{"윈도우즈프로그래밍","3","박현주"}};
+        table1 = new JTable(contents, header);
 
+        Button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    public void Visible(){
+        frame.setVisible(true);
     }
 }
