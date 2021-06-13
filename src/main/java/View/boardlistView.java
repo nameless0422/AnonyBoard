5dd60d770a2c6d2ac7c94b78fbfbdd3a6126be6c
hno130 <hno130@naver.com>
2021-06-13 21:17:41 +0900

    변경사항

--- src/main/java/View/boardlistView.java
+++ src/main/java/View/boardlistView.java
@@ -26,14 +26,14 @@ public class boardlistView {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        String header[] = {"강의명","분반","교수명"};
-        String contents[][] = {{"Java프로그래밍","3","양재동"},{"윈도우즈프로그래밍","3","박현주"}};
-        table1 = new JTable(contents, header);
 
         Button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                String header[] = {"강의명","분반","교수명"};
+                String contents[][] = {{"Java프로그래밍","3","양재동"},{"윈도우즈프로그래밍","3","박현주"}};
+                table1 = new JTable(contents, header);
+                frame.setContentPane(panel);
             }
         });
     }
