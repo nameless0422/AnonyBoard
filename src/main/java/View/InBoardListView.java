e2007bd2b3e62378e2515cf6be1275e4b90cc5cf
GimNoYun <202011616@jbnu.ac.kr>
2021-06-14 17:49:10 +0900

    수정

--- src/main/java/View/InBoardListView.java
+++ src/main/java/View/InBoardListView.java
@@ -3,10 +3,9 @@ package View;
 import Controler.MainProcess;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.*;
+import javax.swing.table.DefaultTableModel;
 
 public class InBoardListView {
     DefaultTableModel model;
@@ -22,14 +21,6 @@ public class InBoardListView {
     private JLabel makeLabel;
     private JTable table1;
 
-
-    String [] columns ={
-            "제목", "시간", "조회수", "추천수"
-    };
-    String[][]data = {
-            {"어쩌고", "13:48", "21", "45"}
-    };
-
     public InBoardListView(MainProcess p) {
         mainProcess = p;
         frame = new JFrame();
@@ -39,36 +30,48 @@ public class InBoardListView {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         scrollPane = new JScrollPane(table1);
         frame.pack();
-        model = new DefaultTableModel(data, columns);
-        JTable table1 = new JTable(model);
-        table1.setModel(model);
-        scrollPane = new JScrollPane(table1);
+//        model = new DefaultTableModel(data, columns);
+        final JTable[] table1 = {new JTable(model)};
+        table1[0].setModel(model);
+        scrollPane = new JScrollPane(table1[0]);
 //        add(scrollPane);
 
-
-
+        Button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed (ActionEvent e){
+                String header[] = {"제목", "시간", "조회수", "추천수"};
+                String contents[][] = {{"어쩌고", "13:24", "32", "3"}, {"윈도우즈프로그래밍", "3", "박현주"}};
+                table1[0] = new JTable(contents, header);
+            }
+        });
         frame.setVisible(true);
     }
 
-    ActionListener al = new ActionListener(){
-        public void actionPerformed(ActionEvent e){
-            model = (DefaultTableModel) table1.getModel();
-            Connection con = null;
-            PreparedStatement pstmt = null;
-            ResultSet rs = null;
-            String[] row = ;
 
-        }
-    }
 
-    public static JTable createTable(){
-        String[] columnNames = {"제목", "시간", "조회수", "추천수"};
-        Object[][] data = {{}};
-        JTable table1 = new JTable(data, columnNames);
-        table1.setFillsViewportHeight(true);
+    public void Visible() {
+//        subLable.setText("반갑습니다! " + mainProcess.User.ID + "님.");
+        frame.setVisible(true);
 
-        return table1;
-    }
+//    ActionListener al = new ActionListener(){
+//        public void actionPerformed(ActionEvent e){
+//            model = (DefaultTableModel) table1.getModel();
+//            Connection con = null;
+//            PreparedStatement pstmt = null;
+//            ResultSet rs = null;
+//            //String[] row = ;
+//
+//        }
+//    }
+
+//    public static JTable createTable(){
+//        String[] columnNames = {"제목", "시간", "조회수", "추천수"};
+//        Object[][] data = {{}};
+//        JTable table1 = new JTable(data, columnNames);
+//        table1.setFillsViewportHeight(true);
+//
+//        return table1;
+//    }
 
 //    public void writeBtn(){
 //        DefaultTableModel model = (DefaultTableModel)table1.getModel();
@@ -88,8 +91,5 @@ public class InBoardListView {
 //
 //        fields[0].requestFocus();
 //    }
-
-    public void Visible(){
-        frame.setVisible(true);
     }
 }
