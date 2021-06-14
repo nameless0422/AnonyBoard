f69a48e51e9c8a647b6b4e9f3a3d6cf8d4b0f628
GimNoYun <202011616@jbnu.ac.kr>
2021-06-14 22:34:45 +0900

    수정

--- src/main/java/View/InBoardListView.java
+++ src/main/java/View/InBoardListView.java
@@ -17,7 +17,7 @@ public class InBoardListView {
     private JPanel panel1;
     private JPanel panel;
     private JButton Button1;
-    private JLabel subLable;
+    private JLabel subLabel;
     private JLabel makeLabel;
     private JTable table1;
 
@@ -30,66 +30,23 @@ public class InBoardListView {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         scrollPane = new JScrollPane(table1);
         frame.pack();
-//        model = new DefaultTableModel(data, columns);
         final JTable[] table1 = {new JTable(model)};
         table1[0].setModel(model);
         scrollPane = new JScrollPane(table1[0]);
-//        add(scrollPane);
 
-        Button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed (ActionEvent e){
-                String header[] = {"제목", "시간", "조회수", "추천수"};
-                String contents[][] = {{"어쩌고", "13:24", "32", "3"}, {"윈도우즈프로그래밍", "3", "박현주"}};
-                table1[0] = new JTable(contents, header);
-            }
-        });
         frame.setVisible(true);
     }
 
+    public static JTable createTable(){
+        String[] columnNames = {"제목", "시간", "조회수", "추천수"};
+        Object[][] data = {{}};
+        JTable table1 = new JTable(data, columnNames);
+        table1.setFillsViewportHeight(true);
 
+        return table1;
+    }
 
     public void Visible() {
-//        subLable.setText("반갑습니다! " + mainProcess.User.ID + "님.");
         frame.setVisible(true);
-
-//    ActionListener al = new ActionListener(){
-//        public void actionPerformed(ActionEvent e){
-//            model = (DefaultTableModel) table1.getModel();
-//            Connection con = null;
-//            PreparedStatement pstmt = null;
-//            ResultSet rs = null;
-//            //String[] row = ;
-//
-//        }
-//    }
-
-//    public static JTable createTable(){
-//        String[] columnNames = {"제목", "시간", "조회수", "추천수"};
-//        Object[][] data = {{}};
-//        JTable table1 = new JTable(data, columnNames);
-//        table1.setFillsViewportHeight(true);
-//
-//        return table1;
-//    }
-
-//    public void writeBtn(){
-//        DefaultTableModel model = (DefaultTableModel)table1.getModel();
-//        String []write = new String[4];
-//        for(int i = 0; i < 4; i++){
-//            if(isInvalidInput(fields[i].getText())){
-//                System.out.println("Invalid Input");
-//                return;
-//            }
-//            write[i]=fields[i].getText();
-//        }
-//        model.addRow(record);
-//
-//        //모든 TextField 비우기
-//        for(int i = 0; i < 4; i++)
-//            fields[i].setText("");
-//
-//        fields[0].requestFocus();
-//    }
     }
 }
