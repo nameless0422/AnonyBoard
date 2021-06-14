ff06922703f3202093cc5fd0ce1f7e284845c1d7
hno130 <hno130@naver.com>
2021-06-14 17:48:42 +0900

    변경사항

--- src/main/java/View/produceView.java
+++ src/main/java/View/produceView.java
@@ -18,12 +18,17 @@ public class produceView {
     public produceView(MainProcess p) {
         mainProcess = p;
         frame = new JFrame();
+        String menu1[] = {"자바","윈프","이산수학"};
+        String menu2[] = {"양재동","박현주","이횽태"};
+        String menu3[] = {"1","2","3"};
+        comboBox1 = new JComboBox(menu1);
+        comboBox2 = new JComboBox(menu2);
+        comboBox3 = new JComboBox(menu3);
         frame.setContentPane(panel1);
         frame.setSize(800, 600);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-
         Button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -31,6 +36,12 @@ public class produceView {
             }
         });
 
+    }
+    public static JComboBox createBox(String a[]) {
+        JComboBox Box1 = new JComboBox(a);
+        Box1.setLightWeightPopupEnabled(true);
+
+        return Box1;
     }
     public void Visible(){
         frame.setVisible(true);
