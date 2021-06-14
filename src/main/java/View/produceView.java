f9bdb9da35f6b4c7819197df5d84c4f791e474e6
hno130 <hno130@naver.com>
2021-06-14 22:33:52 +0900

    변경사항

--- src/main/java/View/produceView.java
+++ src/main/java/View/produceView.java
@@ -1,17 +1,19 @@
 package View;
 
 import Controler.MainProcess;
+import Model.BoardModel;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class produceView {
-    private JComboBox comboBox1;
     private JPanel panel1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
     private JButton Button1;
+    private JScrollPane s1;
+    private JScrollPane s2;
+    private JScrollPane s3;
+
     private MainProcess mainProcess;
     private JFrame frame;
 
@@ -19,18 +21,22 @@ public class produceView {
         mainProcess = p;
         frame = new JFrame();
         String menu1[] = {"자바","윈프","이산수학"};
-        String menu2[] = {"양재동","박현주","이횽태"};
-        String menu3[] = {"1","2","3"};
-        comboBox1 = new JComboBox(menu1);
-        comboBox2 = new JComboBox(menu2);
-        comboBox3 = new JComboBox(menu3);
+        String menu3[] = {"양재동","박현주","이횽태"};
+        String menu2[] = {"1","2","3"};
         frame.setContentPane(panel1);
         frame.setSize(800, 600);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        JComboBox comboBox1 = createBox(menu1);
+        JComboBox comboBox2 = createBox(menu2);
+        JComboBox comboBox3 = createBox(menu3);
+        s1.setViewportView(comboBox1);
+        s2.setViewportView(comboBox2);
+        s3.setViewportView(comboBox3);
         Button1.addActionListener(new ActionListener() {
-            @Override
+            BoardModel bm = new BoardModel(comboBox1.getSelectedItem().toString(),comboBox2.getSelectedIndex(), comboBox3.getSelectedItem().toString());
+
             public void actionPerformed(ActionEvent e) {
                 mainProcess.produceView.frame.setVisible(false);
             }
@@ -39,8 +45,6 @@ public class produceView {
     }
     public static JComboBox createBox(String a[]) {
         JComboBox Box1 = new JComboBox(a);
-        Box1.setLightWeightPopupEnabled(true);
-
         return Box1;
     }
     public void Visible(){
